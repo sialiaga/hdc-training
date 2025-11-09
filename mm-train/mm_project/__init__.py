f208a91a195390fba7007cecf686abe7bbfd1b96
Samuel Aliaga Gleisner <sialiaga@miuandes.cl>
2025-11-09 19:15:16 -0300

    fix: dependencias modulares

--- mm-train/mm_project/__init__.py
+++ mm-train/mm_project/__init__.py
@@ -2,4 +2,4 @@ from .config import ExperimentSettings
 from .dataset import MultimodalDataset
 from .fusion_strategy import FusionModel, CrossAttentionFusion
 from .runner import ExperimentRunner
-from .utils import set_seed, create_network, format_time
+from .utils import set_seed, create_network, format_time, format_time, enviar_notificacion
